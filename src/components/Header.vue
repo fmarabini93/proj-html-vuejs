df7cfc7a913718a5b08b5f930a31bdacc74ca646
fmarabini93 <fmarabini@icloud.com>
2021-06-23 17:16:20 +0200

    touchups-5

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,7 +13,7 @@
                     </div>
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                            <a v-for="voice,index in item" :key="index" class="nav-link active text-end" aria-current="page" :href="`#main-${index}`" style="color: #8b8b8b">{{voice}}</a>
+                            <a v-for="voice,index in item" :key="index" class="nav-link active text-end" aria-current="page" :href="`#main-${index}`" style="color: $lightestgrey">{{voice}}</a>
                         </div>
                     </div>
                 </div>
